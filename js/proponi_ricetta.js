088d1eaf702209d071f2af4d16229fc94df72bd9
lalessio <lucaalessio1994@gmail.com>
2016-05-26 17:41:46 +0200

    file per controllo form completamente funzionante

--- js/proponi_ricetta.js
+++ js/proponi_ricetta.js
@@ -1,55 +1,75 @@
-
 function valida_campi(){
+	if (document.getElementById("err_proponi") !== "") 
+	{
+	    document.getElementById("err_proponi").innerHTML = "";
+	}
+	
+	var nomepiatto = document.getElementById("n_piatto").value;	
+	var nomeauthore = document.getElementById("n_author").value;
+	var descrizione = document.getElementById("n_desc").value;
+	var tempoprep = document.getElementById("n_tempo").value;
+	var numpersone = document.getElementById("n_persone").value;
+	var procedimento = document.getElementById("n_proc").value;
 
+	if(!nomepiatto.replace(/^\s+/g, '').length) {
+            document.getElementById("err_proponi").innerHTML = "Il campo  nome piatto e' obbligatorio*";
+	    document.getElementById("n_piatto").focus();
+	    document.getElementById("n_piatto").select();
+	    return false;
+	} 
 
+	if (!nomeauthore.replace(/^\s+/g, '').length) {
+            document.getElementById("err_proponi").innerHTML = "Il campo autore e' obbligatorio*";
+	    document.getElementById("n_author").focus();
+	    document.getElementById("n_author").select();
+	    return false;
+	}
 
-  var errore = "err_login";
-   //estraggo tutte le variabili:
-   var nomepiatto = document.getElementById("n_piatto").value;   
-   var nomeautore=document.getElementById("n_author").value;
-   var descrizione = document.getElementById("n_desc").value;   
-   var tempo=document.getElementById("n_tempo").value;
-   var numpersone=document.getElementById("n_persone").value;
-   var procedura=document.getElementById("n_proc").value;
-  var areaIngredienti = document.getElementById("n_ingr").value;     
+	if (!descrizione.replace(/^\s+/g, '').length) {
+            document.getElementById("err_proponi").innerHTML = "Il campo descrizione e' obbligatorio*";
+	    document.getElementById("n_desc").focus();
+	    document.getElementById("n_desc").select();
+	    return false;
+	}
 
+	if (!tempoprep.replace(/^\s+/g, '').length) {
+            document.getElementById("err_proponi").innerHTML = "Il campo tempo preparazione e' obbligatorio*";
+	    document.getElementById("n_tempo").focus();
+	    document.getElementById("n_tempo").select();
+	    return false;
+	}
 
-///verifico che i campi non abbiano caratteri speciali:
+	if (!numpersone.replace(/^\s+/g, '').length) {
+            document.getElementById("err_proponi").innerHTML = "Il campo numero persone e' obbligatorio*";
+	    document.getElementById("n_persone").focus();
+	    document.getElementById("n_persone").select();
+	    return false;
+	}
 
+	if (!procedimento.replace(/^\s+/g, '').length) {
+            document.getElementById("err_proponi").innerHTML = "Il campo procedimento e' obbligatorio*";
+	    document.getElementById("n_proc").focus();
+	    document.getElementById("n_proc").select();
+	    return false;
+	}
 
+	var areaIngredienti = document.getElementById("n_ingr").value;
+	var arrayIngredienti = areaIngredienti.split("\n");
+	var arrayLength = arrayIngredienti.length;
 
-   var  regexp= /!@#$%^&*()+=-[]\\\';,.{}|\":<>?/;//controllo espressione regolare caratteri speciali
+	for(var i=0; i < arrayLength; i++)
+	{
+		var currentIng = arrayIngredienti[i];
+		var rigaLength = currentIng.length;
+		var ultimoChar = currentIng.charAt(rigaLength - 1);
+		if(ultimoChar != ";")
+		{	
+			document.getElementById("err_proponi").innerHTML = "campo ingredienti errato, controlla di aver aggiunto un ; al termine di ogni riga!";
+			return false;
+		}	
+	} 
 
-
-        if ((regexp.test(n_piatto) || regexp.test(n_author) || regexp.test(n_desc) || regexp.test(n_tempo) || regexp.test(n_persone)
-         || regexp.test(n_proc)))
-        {
-           document.getElementById(errore).innerHTML = "non sono accettati caratteri speciali*";
-           return false;
-   }
-   else if(!nomepiatto.replace(/^\s+/g, '').length  || !nomeautore.replace(/^\s+/g, '').length || !descrizione.replace(/^\s+/g, '').length
-   			|| !tempo.replace(/^\s+/g, '').length || !numpersone.replace(/^\s+/g, '').length || !procedura.replace(/^\s+/g, '').length)
-  
-   			{
-   				document.getElementById(errore).innerHTML = "tutti i campi sono obbligatori*";
-           	return false;
-   			}
-   			else {//tutti i controlli sono stati effettuati tranne quello dei ;
-   			          var arrayIngredienti = areaIngredienti.value.split("\n");
-                  for(var i=0; i<arrayIngredienti.length; i++)
-                  {
-                    	var currentIng = arrayIngredienti[i];
-                    	var ultimoChar = currentIng.charAt(currentIng.length - 1);
-                    	if(ultimoChar != ";")
-                    	{
-                      		document.getElementById(errore).innerHTML = "campo ingredienti errato, controlla di aver aggiunto un ; al termine di ogni riga!";
-                        	return false;
-                    	}	
-                  } 
-   				      	return true;
-   			}
-
-
-
-   
+	return true;
 }
+ 
+//Last update by Luca 26/05/2016
